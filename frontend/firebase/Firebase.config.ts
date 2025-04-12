5e2dcd3688d1ea10c822d06ba3168fb93be10478
MD Al Amin Hasan <alaminhasanpro@gmail.com>
2025-04-12 22:21:20 +0600

    deployment donme

---
+++ frontend/firebase/Firebase.config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAjCWaI_MK7-NMafXdxY6j2YozwK8T_nE",
+  authDomain: "chapter-house-70055.firebaseapp.com",
+  projectId: "chapter-house-70055",
+  storageBucket: "chapter-house-70055.firebasestorage.app",
+  messagingSenderId: "927562473122",
+  appId: "1:927562473122:web:301614a170638eccb241fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
